e4b532fa39d278880634018fe64c76fa29adf744
Tarun raikwar <raikwartarun72@gmail.com>
2026-08-02 11:30:35 +0530

    initial commit

---
+++ src/main/java/com/tarun/knowledegehub/KnowledegehubApplication.java
@@ -0,0 +1,13 @@
+package com.tarun.knowledegehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledegehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledegehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarun/knowledegehub/KnowledegehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarun.knowledegehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledegehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
